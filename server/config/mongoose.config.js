30ecdaad877040a573ffa69b57fe785fc978d549
Tiyarong Gurung <71687703+tiyaronggurung@users.noreply.github.com>
2022-05-28 00:00:01 -0400

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbName = 'mytickets';
-const dbString = `mongodb+srv://basit740:admin1122@bobisoftprojects.ljxbi.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-// const dbString  =`mongodb+srv://Rochester1995t:12345@cluster0.nfmpd.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const dbString = `mongodb://localhost/${dbName}`;
 
 mongoose
 	.connect(dbString, {
